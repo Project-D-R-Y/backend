72ac18bd7b8435c6b2883d95518d512865f3e0fc
tet tet <teamcorrupted01@gmail.com>
2021-08-09 14:37:41 +0200

    session & Sequelize

--- src/system/App.ts
+++ src/system/App.ts
@@ -2,16 +2,37 @@ import * as express from "express"
 import * as expressSession from "express-session"
 
 import * as cors from "cors"
+import * as cookieParser from "cookie-parser"
+
+import { Sequelize } from "sequelize-typescript"
+const SessionStore = require("express-session-sequelize")(expressSession.Store)
 
 export class App {
     public app : express.Application
+    public sequelize : Sequelize
     public port : number
 
     constructor(port : number, controllers : any) {
+        this.sequelize = new Sequelize({
+            database: "ppbg",
+            dialect: "mysql",
+            username: "root",
+            password: "",
+            models: [__dirname + "/models"]
+        })
+
         this.app = express()
         this.app.use(cors())
         this.app.use(express.json())
         this.app.use(express.urlencoded({extended:true}))
+        this.app.use(expressSession({
+            secret: '',
+            store: new SessionStore({
+                db: this.sequelize
+            }),
+            resave: false,
+            saveUninitialized: true
+        }))
 
         this.port = port
 
