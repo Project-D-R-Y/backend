2771a5e8d16168b1ab26e11903445fb562ada579
tet tet <teamcorrupted01@gmail.com>
2021-08-09 13:55:28 +0200

    add support for controllers

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { App } from "./system-classes/App"
 
-var app = new App(3000)
+var app = new App(3000, [])
 app.listen()
--- src/system-classes/App.ts
+++ src/system-classes/App.ts
@@ -4,9 +4,13 @@ export class App {
     public app : express.Application
     public port : number
 
-    constructor(port : number) {
+    constructor(port : number, controllers : any) {
         this.app = express()
         this.port = port
+
+        controllers.forEach((controller : any) => {
+            this.app.use(controller.prefix, controller.router)
+        })
     }
 
     listen() {
